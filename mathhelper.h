95c03352714a26146cbdd2a3b126fc98d82768f1
rlajustin <justin314kim@gmail.com>
2023-09-06 13:47:47 -0400

    added log10f 	new file:   Makefile 	new file:   generate.cpp 	new file:   log10/Makefile 	new file:   log10/constants.h 	new file:   log10/data.txt 	new file:   log10/helper.h 	new file:   log10/interval.cpp 	new file:   log10/interval.h 	new file:   log10/log10.c 	new file:   log10/oracle.cpp 	new file:   log10/tester/Makefile 	new file:   log10/tester/log 	new file:   log10/tester/log10test 	new file:   log10/tester/log10test.c 	renamed:    log1p/Makefile -> log1p-nearest/Makefile 	renamed:    log1p/constants.h -> log1p-nearest/constants.h 	renamed:    log1p/helper.h -> log1p-nearest/helper.h 	renamed:    log1p/interval.cpp -> log1p-nearest/interval.cpp 	renamed:    log1p/interval.h -> log1p-nearest/interval.h 	renamed:    log1p/log1p.h -> log1p-nearest/log1p.h 	renamed:    log1p/oracle.cpp -> log1p-nearest/oracle.cpp 	renamed:    log1p/polygen.cpp -> log1p-nearest/polygen.cpp 	renamed:    log1p/roundingtest.cpp -> log1p-nearest/roundingtest.cpp 	renamed:    log1p/sorting.cpp -> log1p-nearest/sorting.cpp 	renamed:    log1p/test.c -> log1p-nearest/test.c 	new file:   mathhelper.h 	new file:   sorting.cpp

---
+++ mathhelper.h
@@ -0,0 +1,42 @@
+#ifndef MATHHELPER_H
+#define MATHHELPER_H
+
+#include <stdbool.h>
+#include <math.h>
+
+typedef union {
+	double d;
+	unsigned long long x;
+} Double;
+
+static inline double multiply(double a, double b)
+{
+	double x = a*b;
+	Double retval;
+	retval.d = x;
+	Double compare = {fma(a,b,-x)};
+	//handle when compare is positive or negative 0.0
+	retval.x -= (((compare.x ^ retval.x) > 0x8000000000000000) &&
+			((compare.x<<1)!=0x0));
+	return retval.d;
+}
+
+static inline double add(double a, double b)
+{
+	union {double d; unsigned long x;} ba = {a}, bb = {b}, retval = {a+b};
+	int alb = ((ba.x & 0x7FFFFFFFFFFFFFFF) < (bb.x & 0x7FFFFFFFFFFFFFFF));
+	unsigned long mask = ba.x ^ bb.x;
+
+	ba.x = ba.x ^ (mask * alb);
+	bb.x = bb.x ^ (mask * alb); 
+
+	ba.d = retval.d - ba.d;
+
+	bb.d -= ba.d;
+
+	retval.x -= (((bb.x ^ retval.x)>0x8000000000000000) &&
+			((bb.x<<1)!=0x0));
+	return retval.d;
+}
+
+#endif
